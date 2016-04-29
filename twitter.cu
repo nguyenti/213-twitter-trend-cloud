f547894cac28aa28c7a41e29164a8aabd9099fc4
VBashlovkina <bashlovk@grinnell.edu>
2016-04-29 08:54:11 -0500

    set up the pipe in the child and parse json trends

--- twitter.cu
+++ twitter.cu
@@ -34,20 +34,38 @@ int main(int argc, char** argv) {
          fd[0] will be input end
          fd[1] will be output end */
 
-
+      // open the pipe
       if (pipe (fd) < 0){
         perror("pipe error");
-        exit (2);
+        exit(1);
       }
 
       int rc = fork();
       if (rc < 0) { // error
         perror("Fork failed!");
-        exit(2);
+        exit(1);
       } else if (rc == 0) { // child
-        
-      } else { // parent
+        // close input, leave output open
+        close (fd[0]);
+        // set stdout to pipe
+        if (fd[1] !=  STDOUT_FILENO) {
+          if (dup2(fd[1], STDOUT_FILENO) != STDOUT_FILENO){
+            perror("dup2 error for standard output");
+            exit(1);
+          }
+          close(fd[1]); /* not needed after dup2 */
+        }
+        // now stdout goes to the pipe
 
+        // TODO: exec curl
+
+        // TESTING: exec cat
+        execvp("cat", {"cat", "trends.json"});
+        perror("execvp failed");
+      } else { // parent
+        // read trends from stdout
+
+        
       }
     }
     
@@ -72,6 +90,80 @@ int main(int argc, char** argv) {
   
   return 0;
 }
+
+
+// Returns the size of the trends array
+// Make sure to allocate the trends array!
+// char ** trends = (char **)malloc(sizeof(char *) * NUMTRENDS);
+size_t read_trends(char ** trends) {
+
+  // for using getline
+  static char* line = NULL;
+  static size_t line_maxlen = 0;
+  ssize_t line_length;
+  
+  // Loop until we read one valid json array or reach the end of the input
+  while((line_length = getline(&line, &line_maxlen, stdin)) > 0) {
+    // Parse the JSON body
+    json_error_t error;
+    json_t* root = json_loads(line, 0, &error);
+  
+    // Skip over lines with errors
+    if(!root) continue;
+  
+    // Skip over lines that aren't JSON nonempty arrays
+    if(!json_is_array(root) || json_array_size(root) < 1) {
+      json_decref(root);
+      continue;
+    }
+
+    // Get the first object in the array
+    json_t * first_object = json_array_get(root, 0);
+
+    // Get the json trends array
+    json_t * json_trends_array = json_object_get(first_object, "trends");
+
+    size_t arr_size;
+     // Make sure 'trends' is a nonempty array
+    if(!json_is_array(json_trends_array) ||
+       (arr_size = json_array_size(json_trends_array)) < 1) {
+      json_decref(root);
+      continue;
+    }
+
+    size_t i;
+    // Read every trend into a regular C array
+    for (i = 0; i < arr_size &&  i < NUMTRENDS; i++) {
+      json_t * json_trend_obj = json_array_get(json_trends_array, i);
+      
+      // Get the name of the trend
+      json_t* text = json_object_get(json_trend_obj, "name");
+  
+      // If there was no text, skip this t
+      if(!json_is_string(text)) {
+        json_decref(root);
+        continue;
+      }
+  
+      // Get the string out of the JSON text value
+      const char* json_text = json_string_value(text);
+  
+      // Got a tweet! Copy just the tweet text to an allocated buffer
+      trends[i] = (char*)malloc(sizeof(char) * (line_length + 1));//TODO: ???
+      strcpy(trends[i], json_text);
+    
+      // Release this reference to the JSON object
+      json_decref(json_trend_obj);
+
+    }
+    
+    // Return the number of trends read
+    return i;
+  }
+  
+  // Ran out of input. Just return NULL
+  return NULL;
+} // read_trends
 
 
 char* read_tweet() {
@@ -120,3 +212,4 @@ char* read_tweet() {
   // Ran out of input. Just return NULL
   return NULL;
 }
+
